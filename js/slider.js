d9bb9ba2af236cef363cc1d838becd6db3b0fc94
Chris Toft Christensen <spam@chrisc.dk>
2015-04-29 16:53:37 +0200

    Bullets now represents the current page

--- js/slider.js
+++ js/slider.js
@@ -3,4 +3,9 @@ $(document).ready(function () {
         var page = $(this).index("li");
         $('.slide-container').slick("slickGoTo", page);
     });
+    $('.slide-container').on('afterChange', function (event, slick, direction) {
+        var currentSlide = $('.slide-container').slick("slickCurrentSlide");
+        $("#bullets li").toggleClass("active", false);
+        $("#bullets li:nth-child(" + (currentSlide + 1) + ")" ).toggleClass("active", true);
+    });
 });
